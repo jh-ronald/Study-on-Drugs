0db21449ecced0d3c6ae75678045010310e42c97
jhronald <ronaldjhchow@gmail.com>
2022-12-21 14:42:37 +0800

    WP1

---
+++ dataset-dl.r
@@ -0,0 +1,12 @@
+dl <- tempfile()
+download.file(
+    "https://raw.githubusercontent.com/jh-ronald/Study-on-Drugs/master/drugs_side_effects_drugs_com.csv",
+    destfile = dl)
+
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
+library(tidyverse)
+library(data.table)
+
+temp <- read_csv(dl, show_col_types = "FALSE")
+drugs <- data.table(temp)
