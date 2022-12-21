ba575053af5f4642bfe3a492db9196174b13ceef
jhronald <ronaldjhchow@gmail.com>
2022-12-21 18:11:10 +0800

    Fix on Column Class

--- dataset-dl.r
+++ dataset-dl.r
@@ -1,7 +1,7 @@
 dl <- tempfile()
 download.file(
-    "https://raw.githubusercontent.com/jh-ronald/Study-on-Drugs/master/drugs_side_effects_drugs_com.csv",
-    destfile = dl)
+  "https://raw.githubusercontent.com/jh-ronald/Study-on-Drugs/master/drugs_side_effects_drugs_com.csv",
+  destfile = dl)
 
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
@@ -10,3 +10,5 @@ library(data.table)
 
 temp <- read_csv(dl, show_col_types = "FALSE")
 drugs <- data.table(temp)
+
+col_class <- data.frame(sapply(drugs, class))
